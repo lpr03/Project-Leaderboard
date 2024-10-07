77b48f11cf02e548850f0be55bbd4fb8801bddb3
harika1193 <harikamallepally11@gmail.com>
2024-10-07 14:39:08 +0530

    time out changes

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,48 +1,51 @@
-import Cookies from 'cookies'
+import Cookies from 'cookies';
 import clientPromise from "../../../lib/mongodb";
 import { updateQuestionsSolved } from './scrape';
+import { createHash } from 'node:crypto';
 
-const { createHash } = require('node:crypto');
 export default async function handler(req, res) {
-  if (req.method == "POST") {
-    const email = req.body['email']
-    const username = req.body['username']
-    const password = req.body['password']
-    const passwordagain = req.body['passwordagain']
-    const lt_username = req.body['lt_username']
-    const gfg_username = req.body['gfg_username']
-    const cc_username = req.body['cc_username']
+    if (req.method === "POST") {
+        const { email, username, password, passwordagain, lt_username, gfg_username, cc_username } = req.body;
 
-    if (password != passwordagain) {
-      res.redirect("/signup?msg=The two passwords don't match");
-      return;
+        if (password !== passwordagain) {
+            res.redirect("/signup?msg=The two passwords don't match");
+            return;
+        }
+
+        try {
+            const client = await clientPromise;
+            const db = client.db("Users");
+            const userExists = await db.collection("Profiles").findOne({ Username: username });
+
+            if (userExists) {
+                res.redirect("/signup?msg=A user already has this username");
+                return;
+            }
+
+            const password_hash = createHash('sha256').update(password).digest('hex');
+            const currentDate = new Date().toUTCString();
+            const bodyObject = {
+                Email: email,
+                Username: username,
+                Password: password_hash,
+                Created: currentDate,
+                Lt_username: lt_username,
+                GFG_username: gfg_username,
+                CC_username: cc_username,
+                resetPasswordToken: null,
+                resetPasswordExpires: null,
+            };
+
+            await db.collection("Profiles").insertOne(bodyObject);
+            const cookies = new Cookies(req, res);
+            cookies.set('username', username);
+            await updateQuestionsSolved();
+            res.redirect("/");
+        } catch (error) {
+            console.error("Error during signup:", error);
+            res.redirect("/signup?msg=Error occurred, please try again.");
+        }
+    } else {
+        res.redirect("/");
     }
-    const client = await clientPromise;
-    const db = client.db("Users");
-    const users = await db.collection("Profiles").find({ "Username": username }).toArray();
-    if (users.length > 0) {
-      res.redirect("/signup?msg=A user already has this username");
-      return;
-    }
-    const password_hash = createHash('sha256').update(password).digest('hex');
-    const currentDate = new Date().toUTCString();
-    const bodyObject = {
-      Email: email,
-      Username: username,
-      Password: password_hash,
-      Created: currentDate,
-      Lt_username: lt_username,
-      GFG_username: gfg_username,
-      CC_username: cc_username,
-      resetPasswordToken: null,
-      resetPasswordExpires: null,
-    }
-    await db.collection("Profiles").insertOne(bodyObject);
-    const cookies = new Cookies(req, res)
-    cookies.set('username', username)
-    await updateQuestionsSolved();
-    res.redirect("/")
-  } else {
-    res.redirect("/")
-  }
 }
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LayoutBeforeLogin from '../components/LayoutBeforeLogin';
-import { getCookie } from 'cookies-next';
 import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
@@ -9,90 +8,100 @@ export default function SignupPage({ username }) {
     const router = useRouter();
     const { msg } = router.query;
 
+    const [loading, setLoading] = useState(false);
     const [passwordVisible, setPasswordVisible] = useState(false);
     const [passwordAgainVisible, setPasswordAgainVisible] = useState(false);
 
-    const togglePasswordVisibility = () => {
-        setPasswordVisible(!passwordVisible);
+    const togglePasswordVisibility = () => setPasswordVisible(!passwordVisible);
+    const togglePasswordAgainVisibility = () => setPasswordAgainVisible(!passwordAgainVisible);
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const formData = new FormData(e.target);
+        
+        try {
+            const response = await fetch('/api/signup', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                router.push('/');
+            } else {
+                const error = await response.text();
+                console.error('Error:', error);
+                router.push(`/signup?msg=${encodeURIComponent(error)}`);
+            }
+        } catch (error) {
+            console.error('Signup error:', error);
+        } finally {
+            setLoading(false);
+        }
     };
 
-    const togglePasswordAgainVisibility = () => {
-        setPasswordAgainVisible(!passwordAgainVisible);
-    };
+    useEffect(() => {
+        if (loading) {
+            const timeoutId = setTimeout(() => {
+                window.location.reload();
+            }, 9000);
+
+            return () => clearTimeout(timeoutId);
+        }
+    }, [loading]);
 
     return (
         <LayoutBeforeLogin pageTitle="Signup">
-            {msg ? <h3 className="red">{msg}</h3> : <></>}
+            {msg && <h3 className="red">{msg}</h3>}
             <main className="main-content">
                 <div className="form-container">
-                    <h2 style={{textAlign:"center"}}>SignUp</h2>
-                    <form action="/api/signup" method="POST">
+                    <h2 style={{ textAlign: 'center' }}>SignUp</h2>
+                    <form onSubmit={handleFormSubmit}>
                         <div className="form-group">
-                            <input name="email" id="email" type="email" placeholder="Email" required />
+                            <input name="email" type="email" placeholder="Email" required />
                         </div>
                         <div className="form-group">
-                            <input minLength="3" name="username" id="username" type="text" placeholder="Username" required />
+                            <input minLength="3" name="username" type="text" placeholder="Username" required />
                         </div>
                         <div className="password-container form-group" style={{ position: 'relative' }}>
                             <input
                                 type={passwordVisible ? 'text' : 'password'}
-                                id="password"
-                                className="password-input"
                                 name="password"
                                 pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=])[A-Za-z\d@#$%^&+=]{8,}$"
                                 placeholder="Password"
-                                title="Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, number and special character."
+                                title="Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, number, and special character."
                                 required
                             />
-                            <span
-                                onClick={togglePasswordVisibility}
-                                style={{
-                                    position: 'absolute',
-                                    right: '10px',
-                                    top: '50%',
-                                    transform: 'translateY(-50%)',
-                                    cursor: 'pointer',
-                                    width: '18px',
-                                }}
-                            >
+                            <span onClick={togglePasswordVisibility} style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)', cursor: 'pointer', width: '18px' }}>
                                 <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
                             </span>
                         </div>
                         <div className="password-container form-group" style={{ position: 'relative' }}>
                             <input
                                 type={passwordAgainVisible ? 'text' : 'password'}
-                                id="passwordagain"
                                 name="passwordagain"
                                 pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=])[A-Za-z\d@#$%^&+=]{8,}$"
                                 placeholder="Password Again"
-                                title="Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, number and special character."
                                 required
                             />
-                            <span
-                                onClick={togglePasswordAgainVisibility}
-                                style={{
-                                    position: 'absolute',
-                                    right: '10px',
-                                    top: '50%',
-                                    transform: 'translateY(-50%)',
-                                    cursor: 'pointer',
-                                    width: '18px',
-                                }}
-                            >
+                            <span onClick={togglePasswordAgainVisibility} style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)', cursor: 'pointer', width: '18px' }}>
                                 <FontAwesomeIcon icon={passwordAgainVisible ? faEyeSlash : faEye} />
                             </span>
                         </div>
                         <div className="form-group">
-                            <input minLength="3" name="lt_username" id="lt_username" type="text" placeholder="Leetcode username" />
+                            <input minLength="3" name="lt_username" type="text" placeholder="Leetcode username" />
                         </div>
                         <div className="form-group">
-                            <input minLength="3" name="gfg_username" id="gfg_username" type="text" placeholder="GeekForGeeks username" />
+                            <input minLength="3" name="gfg_username" type="text" placeholder="GeekForGeeks username" />
                         </div>
                         <div className="form-group">
-                            <input minLength="3" name="cc_username" id="cc_username" type="text" placeholder="Codechef username" />
+                            <input minLength="3" name="cc_username" type="text" placeholder="Codechef username" />
                         </div>
                         <div className="form-group">
-                            <button type="submit">Sign Up</button>
+                            <button type="submit" disabled={loading}>
+                                {loading ? 'Signing Up...' : 'Sign Up'}
+                            </button>
                         </div>
                     </form>
                 </div>
@@ -104,8 +113,8 @@ export default function SignupPage({ username }) {
 export async function getServerSideProps(context) {
     const req = context.req;
     const res = context.res;
-    var username = getCookie('username', { req, res });
-    if (username != undefined) {
+    const username = getCookie('username', { req, res });
+    if (username) {
         return {
             redirect: {
                 permanent: false,
